6c806171187c848bd96f227a0df951b26c5647ad
catoocraft <catoocraft@gmail.com>
2021-04-03 21:51:02 +0900

    apply splash theme

--- app/src/main/java/com/catoocraft/splashstudy/MainActivity.kt
+++ app/src/main/java/com/catoocraft/splashstudy/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.catoocraft.splashstudy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        try {
+            Thread.sleep(1000)
+        } catch (e: InterruptedException) {
+            throw RuntimeException(e)
+        }
+
+        setTheme(R.style.Theme_SplashStudy)
         setContentView(R.layout.activity_main)
     }
 }
